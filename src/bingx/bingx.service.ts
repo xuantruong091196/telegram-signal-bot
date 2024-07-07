a3952dca0e20637b04ef80193fc6f61346cdec4a
xuantruongdeveloper0911 <xuantruongdeveloper0911@gmail.com>
2024-07-08 00:34:12 +0700

    Fix

--- src/bingx/bingx.service.ts
+++ src/bingx/bingx.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { bingx } from 'src/config/constant';
 import { LEVERAGE } from 'src/config/constant';
 
@@ -12,14 +12,15 @@ export class BingxService {
     takeProfit: string;
     stopLoss: string;
   }) {
-    await bingx.setLeverage(LEVERAGE, params.symbol, params);
-    await bingx.createOrder(
-      params.symbol,
-      'limit',
-      params.positionSide.toUppercase(),
-      5,
-      params.price,
-      params,
-    );
+    // await bingx.setLeverage(LEVERAGE, params.symbol, params);
+    // await bingx.createOrder(
+    //   params.symbol,
+    //   'limit',
+    //   params.positionSide.toUppercase(),
+    //   5,
+    //   params.price,
+    //   params,
+    // );
+    Logger.log(params)
   }
 }
